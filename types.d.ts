ef0e52280a1f9549db714f6291d8c48945391e9e
Maciej Garncarski <maciejgarncarski@protonmail.com>
2022-12-10 22:08:16 +0100

    chore: add svg module for ts

--- types.d.ts
+++ types.d.ts
@@ -7,3 +7,11 @@ declare module '*.module.scss' {
   const classes: { [key: string]: string };
   export default classes;
 }
+
+declare module '*.svg' {
+  const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  const content: string;
+
+  export { ReactComponent };
+  export default content;
+}
