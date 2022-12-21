c69db61a1133e49e12bc30c19add3ea5b232367d
Maciej Garncarski <maciejgarncarski@protonmail.com>
2022-12-21 22:39:12 +0100

    feat: cookie alert

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,7 +1,9 @@
-import { ReactNode } from 'react';
+import { hasCookie, setCookie } from 'cookies-next';
+import { ReactNode, useState } from 'react';
 
 import styles from './layout.module.scss';
 
+import { Button } from '@/components/atoms/button/Button';
 import { CompleteSignUp } from '@/components/molecules/completeSignUp/CompleteSignUp';
 import { Header } from '@/components/organisms/header/Header';
 import { useScreenWidth } from '@/components/organisms/header/useScreenWidth';
@@ -13,37 +15,50 @@ export type Children = {
   children: ReactNode;
 };
 
-export const Layout = ({ children }: Children) => {
+type LayoutProps = Children;
+
+const COOKIES_ACCEPTED = 'cookiesAccepted' as const;
+
+export const Layout = ({ children }: LayoutProps) => {
+  const isCookieAccepted = hasCookie(COOKIES_ACCEPTED);
+  const [isCookiesOpen, setIsCookiesOpen] = useState<boolean>(!isCookieAccepted);
+
   useScrollPosition();
   useScreenWidth();
 
   const { session, status } = useAuth();
-  const { data, isLoading } = useAccount({ id: session?.user?.id });
+  const { data } = useAccount({ id: session?.user?.id });
 
-  if (isLoading || !data) {
-    return (
-      <div>
-        <Header />
-        <div className={styles.children}>{children}</div>
-      </div>
-    );
-  }
+  const isNotCompleted = !data?.user.username && status === 'authenticated';
 
-  if (!data?.user.username && status === 'authenticated') {
-    return (
-      <div>
-        <Header />
-        <div className={styles.children}>
-          <CompleteSignUp />
-        </div>
-      </div>
-    );
-  }
+  const handleClose = () => {
+    setIsCookiesOpen(false);
+  };
+
+  const handleCookies = () => {
+    setCookie(COOKIES_ACCEPTED, true);
+    handleClose();
+  };
 
   return (
     <div>
       <Header />
-      <div className={styles.children}>{children}</div>
+      <div className={styles.children}>
+        {isCookiesOpen && (
+          <div role='dialog' className={styles.cookies}>
+            <p>By using this app, you accept necessary cookies.</p>
+            <div className={styles.cookiesButtons}>
+              <Button type='button' variant='secondary' onClick={handleClose}>
+                Close
+              </Button>
+              <Button type='button' onClick={handleCookies}>
+                Don&apos;t show again
+              </Button>
+            </div>
+          </div>
+        )}
+        {isNotCompleted ? <CompleteSignUp /> : <>{children}</>}
+      </div>
     </div>
   );
 };
