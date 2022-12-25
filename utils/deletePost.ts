df00a5f0b46d80a33e4dfcea6527515058780097
Maciej Garncarski <maciejgarncarski@protonmail.com>
2022-12-25 15:10:43 +0100

    fix: remove console.log

--- utils/deletePost.ts
+++ utils/deletePost.ts
@@ -19,7 +19,6 @@ export const deletePost = async (req: NextApiRequest, res: NextApiResponse) => {
     });
 
     const isAbleToDelete = post?.author_id === session?.user?.id;
-    console.log(post?.author_id, session?.user?.id);
 
     if (!isAbleToDelete) {
       res.status(401).send({ status: 'unauthorized' });
