61a82144534e2affca48484f53e6af4387051d4e
Maciej Garncarski <maciejgarncarski@protonmail.com>
2023-04-22 10:18:36 +0200

    fix: signin

--- src/utils/signIn.ts
+++ src/utils/signIn.ts
@@ -1,9 +1,7 @@
 import { QueryClient } from '@tanstack/react-query';
-import axios from 'axios';
-import Router from 'next/router';
 import { z } from 'zod';
 
-import { clientEnv } from '@/src/utils/env';
+import { apiClient } from '@/src/utils/apis/apiClient';
 
 export const SignInSchema = z.object({
   email: z.string().email({ message: 'Invalid email.' }),
@@ -17,25 +15,10 @@ type SignInCredentials = {
 } & SignInFormValues;
 
 export const signInCredentials = async ({ email, password, queryClient }: SignInCredentials) => {
-  const { data } = await axios.post(
-    `${clientEnv.NEXT_PUBLIC_API_ROOT}api/auth/login`,
-    {
-      email,
-      password,
-    },
-    {
-      withCredentials: true,
-    },
-  );
+  await apiClient.post('auth/login', {
+    email,
+    password,
+  });
 
   queryClient.invalidateQueries(['session']);
-
-  if (data.redirect) {
-    Router.push(data.redirect);
-  }
-};
-
-export const signInGoogle = () => {
-  //TODO change to link href
-  window.location.href = `${clientEnv.NEXT_PUBLIC_API_ROOT}auth/google`;
 };
