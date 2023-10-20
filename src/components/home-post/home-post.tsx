4cbf13edffd3f3c88e7188dcc027d6d83d2c04bb
Maciej Garncarski <maciejgarncarski@protonmail.com>
2023-10-20 22:05:56 +0200

    feat: add typechecking husky hook

--- src/components/home-post/home-post.tsx
+++ src/components/home-post/home-post.tsx
@@ -1,8 +1,6 @@
-import { usePost } from '@/src/components/pages/account/use-post';
 import { PostFooter } from '@/src/components/post/post-footer/post-footer';
 import { PostHeader } from '@/src/components/post/post-header/post-header';
 import { PostImagesCarousel } from '@/src/components/post/post-images-carousel/post-images-carousel';
-import { PostPlaceholder } from '@/src/components/post/post-placeholder/post-placeholder';
 
 import styles from './home-post.module.scss';
 
@@ -12,22 +10,10 @@ type Props = {
 };
 
 export const HomePost = ({ postId, priority }: Props) => {
-  const { data: post, isPending } = usePost({ postId });
-
-  if (isPending || !post) {
-    return (
-      <li>
-        <PostPlaceholder />
-      </li>
-    );
-  }
-
-  const { authorId, createdAt } = post;
-
   return (
     <li>
       <article className={styles.homePost}>
-        <PostHeader authorId={authorId} createdAt={createdAt} postId={postId} />
+        <PostHeader postId={postId} />
         <PostImagesCarousel postId={postId} priority={priority} />
         <PostFooter postId={postId} />
       </article>
