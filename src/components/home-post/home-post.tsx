0289778e95a10b598db59390ca1f641b1df90476
Maciej Garncarski <maciejgarncarski@protonmail.com>
2023-10-21 15:48:09 +0200

    fix: post placeholder loading

--- src/components/home-post/home-post.tsx
+++ src/components/home-post/home-post.tsx
@@ -1,6 +1,8 @@
+import { usePost } from '@/src/components/pages/account/use-post';
 import { PostFooter } from '@/src/components/post/post-footer/post-footer';
 import { PostHeader } from '@/src/components/post/post-header/post-header';
 import { PostImagesCarousel } from '@/src/components/post/post-images-carousel/post-images-carousel';
+import { PostPlaceholder } from '@/src/components/post/post-placeholder/post-placeholder';
 
 import styles from './home-post.module.scss';
 
@@ -10,6 +12,12 @@ type Props = {
 };
 
 export const HomePost = ({ postId, priority }: Props) => {
+  const { isPending } = usePost({ postId });
+
+  if (isPending) {
+    return <PostPlaceholder />;
+  }
+
   return (
     <li>
       <article className={styles.homePost}>
