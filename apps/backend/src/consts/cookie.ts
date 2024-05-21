878b55784566b0bb0e9dca6a86a9f9f4ea677ae0
Maciej Garncarski <maciejgarncarski@protonmail.com>
2024-05-21 19:55:48 +0200

    fix: lax to strict

--- apps/backend/src/consts/cookie.ts
+++ apps/backend/src/consts/cookie.ts
@@ -6,6 +6,6 @@ import { envVariables } from '../utils/envVariables.js';
 export const cookie: CookieOptions = {
   httpOnly: true,
   maxAge: ms('2 days'),
-  sameSite: envVariables.PRODUCTION === 'true' ? 'none' : 'lax',
+  sameSite: envVariables.PRODUCTION === 'true' ? 'none' : 'strict',
   secure: envVariables.PRODUCTION === 'true' ? true : false,
 };
