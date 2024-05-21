c8cf5b71f07e185ae3fa83ce79b5c261609f456c
Maciej Garncarski <maciejgarncarski@protonmail.com>
2024-05-21 15:21:57 +0200

    chore: remove prefetching for debugging

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -1,48 +1,34 @@
-import {
-  dehydrate,
-  HydrationBoundary,
-  QueryClient,
-} from "@tanstack/react-query";
-
-import { userQueryOptions } from "@/hooks/use-user";
-
-import { getPostQueryOptions } from "@/components/pages/account/use-post";
 import { Home } from "@/components/pages/home/home";
-import { getHomepagePostsOptions } from "@/components/pages/home/use-homepage-posts";
 
 export default async function HomePage() {
-  const queryClient = new QueryClient();
+  // const queryClient = new QueryClient();
 
-  try {
-    const data = await queryClient.fetchInfiniteQuery(getHomepagePostsOptions);
+  // try {
+  //   const data = await queryClient.fetchInfiniteQuery(getHomepagePostsOptions);
 
-    const postIds = data.pages.map(({ data }) => data.map(({ id }) => id))[0];
-    const authorIds = data.pages.map(({ data }) =>
-      data.map(({ authorId }) => authorId)
-    )[0];
+  //   const postIds = data.pages.map(({ data }) => data.map(({ id }) => id))[0];
+  //   const authorIds = data.pages.map(({ data }) =>
+  //     data.map(({ authorId }) => authorId)
+  //   )[0];
 
-    await Promise.all(
-      postIds.map((id) => {
-        return queryClient.prefetchQuery(getPostQueryOptions(id));
-      })
-    );
+  //   await Promise.all(
+  //     postIds.map((id) => {
+  //       return queryClient.prefetchQuery(getPostQueryOptions(id));
+  //     })
+  //   );
 
-    await Promise.all(
-      authorIds.map((authorId) => {
-        return queryClient.prefetchQuery(userQueryOptions(authorId));
-      })
-    );
+  //   await Promise.all(
+  //     authorIds.map((authorId) => {
+  //       return queryClient.prefetchQuery(userQueryOptions(authorId));
+  //     })
+  //   );
 
-    // eslint-disable-next-line no-console
-    console.log({ data });
-  } catch (error) {
-    // eslint-disable-next-line no-console
-    console.log({ error });
-  }
+  // } catch (error) {
+  // }
 
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <Home />
-    </HydrationBoundary>
+    // <HydrationBoundary state={dehydrate(queryClient)}>
+    <Home />
+    // </HydrationBoundary>
   );
 }
