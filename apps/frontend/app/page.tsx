2052b6ce759f2121159bef7e427eeb7fd829cab0
Maciej Garncarski <maciejgarncarski@protonmail.com>
2024-05-24 08:53:16 +0200

    fix: remove try catch

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -13,26 +13,24 @@ import { getHomepagePostsOptions } from "@/components/pages/home/use-homepage-po
 export default async function HomePage() {
   const queryClient = new QueryClient();
 
-  try {
-    const data = await queryClient.fetchInfiniteQuery(getHomepagePostsOptions);
+  const data = await queryClient.fetchInfiniteQuery(getHomepagePostsOptions);
 
-    const postIds = data.pages.map(({ data }) => data.map(({ id }) => id))[0];
-    const authorIds = data.pages.map(({ data }) =>
-      data.map(({ authorId }) => authorId)
-    )[0];
+  const postIds = data.pages.map(({ data }) => data.map(({ id }) => id))[0];
+  const authorIds = data.pages.map(({ data }) =>
+    data.map(({ authorId }) => authorId)
+  )[0];
 
-    await Promise.all(
-      postIds.map((id) => {
-        return queryClient.prefetchQuery(getPostQueryOptions(id));
-      })
-    );
+  await Promise.all(
+    postIds.map((id) => {
+      return queryClient.prefetchQuery(getPostQueryOptions(id));
+    })
+  );
 
-    await Promise.all(
-      authorIds.map((authorId) => {
-        return queryClient.prefetchQuery(userQueryOptions(authorId));
-      })
-    );
-  } catch (error) {}
+  await Promise.all(
+    authorIds.map((authorId) => {
+      return queryClient.prefetchQuery(userQueryOptions(authorId));
+    })
+  );
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
