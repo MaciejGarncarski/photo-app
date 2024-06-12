22836203b1a0103bd64405aea05cb6919c27e98e
Maciej Garncarski <maciejgarncarski@protonmail.com>
2024-06-12 12:10:34 +0200

    feat: loading

--- apps/frontend/components/home-posts-list/home-posts-list.tsx
+++ apps/frontend/components/home-posts-list/home-posts-list.tsx
@@ -1,12 +1,12 @@
 "use client";
 
 import { PlusCircle } from "@phosphor-icons/react";
+import { Suspense } from "react";
 
 import { useAuth } from "@/hooks/use-auth";
 import { useInfiniteScroll } from "@/hooks/use-infinite-scroll";
 
 import { ButtonLink } from "@/components/buttons/button-link/button-link";
-import { FetchErrorMessage } from "@/components/fetch-error-message/fetch-error-message";
 import { HomePost } from "@/components/home-post/home-post";
 import { Loader } from "@/components/loader/loader";
 import { useHomepagePosts } from "@/components/pages/home/use-homepage-posts";
@@ -15,9 +15,7 @@ import { PostPlaceholder } from "@/components/post/post-placeholder/post-placeho
 import styles from "./home-posts-list.module.scss";
 
 export const HomePostsList = () => {
-  const { data, isPending, hasNextPage, fetchNextPage, isError } =
-    useHomepagePosts();
-
+  const { data, isPending, hasNextPage, fetchNextPage } = useHomepagePosts();
   const { isSignedIn } = useAuth();
 
   const { ref } = useInfiniteScroll({
@@ -25,20 +23,6 @@ export const HomePostsList = () => {
     fetchNextPage,
     enabled: true,
   });
-
-  if (isError) {
-    return <FetchErrorMessage message="Cannot fetch data." />;
-  }
-
-  if (isPending) {
-    return (
-      <div className={styles.posts}>
-        <PostPlaceholder />
-        <PostPlaceholder />
-        <PostPlaceholder />
-      </div>
-    );
-  }
 
   const noPosts = data?.pages[0] && data.pages[0].postsCount < 1;
 
@@ -58,12 +42,20 @@ export const HomePostsList = () => {
 
   return (
     <div className={styles.posts}>
-      {data?.pages.map((page) => {
-        return page.data.map(({ id }, idx) => {
-          return <HomePost priority={idx <= 1} key={id} postId={id} />;
-        });
-      })}
-
+      <Suspense
+        fallback={
+          <div className={styles.posts}>
+            <PostPlaceholder />
+            <PostPlaceholder />
+          </div>
+        }
+      >
+        {data?.pages.map((page) => {
+          return page.data.map(({ id }, idx) => {
+            return <HomePost priority={idx <= 1} key={id} postId={id} />;
+          });
+        })}
+      </Suspense>
       {!isPending && hasNextPage && (
         <div role="status" ref={ref}>
           <Loader size="small" color="primary" />
