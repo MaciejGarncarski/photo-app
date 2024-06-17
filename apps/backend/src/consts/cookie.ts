833c87afc34a3cdd40e6f58ab55c1ba14c694b04
Maciej Garncarski <maciejgarncarski@protonmail.com>
2024-06-17 12:20:37 +0200

    fix: package json

--- apps/backend/src/consts/cookie.ts
+++ apps/backend/src/consts/cookie.ts
@@ -1,7 +1,7 @@
 import type { CookieOptions } from '@fastify/session'
 import ms from 'ms'
 
-const isProd = process.env.NODE_ENV === 'production'
+const isProd = process.env.PRODUCTION === 'true'
 
 export const cookie: CookieOptions = {
 	httpOnly: true,
