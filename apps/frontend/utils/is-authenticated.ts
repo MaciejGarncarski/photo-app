01d9167c6c20ae001b6e55ac66c511cc79226ca3
Maciej Garncarski <maciejgarncarski@protonmail.com>
2024-07-02 15:21:13 +0200

    chore: add no-cache

--- apps/frontend/utils/is-authenticated.ts
+++ apps/frontend/utils/is-authenticated.ts
@@ -3,12 +3,16 @@ import { getSessionUser } from '@/services/auth.service'
 export const isAuthenticated = async () => {
 	try {
 		const { cookies } = await import('next/headers')
+		// eslint-disable-next-line no-console
+		console.log(`sessionId=${cookies().get('sessionId')?.value}`)
+
 		const response = await getSessionUser(
 			{},
 			{
 				headers: {
 					Cookie: `sessionId=${cookies().get('sessionId')?.value}`,
 				},
+				cache: 'no-store',
 			},
 		)
 
