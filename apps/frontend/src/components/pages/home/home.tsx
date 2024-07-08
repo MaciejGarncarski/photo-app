a12086c19a85837119b93eec8b9fa193b5aacb48
Maciej Garncarski <maciejgarncarski@protonmail.com>
2024-07-08 14:33:26 +0200

    fix: new post notification import

--- apps/frontend/src/components/pages/home/home.tsx
+++ apps/frontend/src/components/pages/home/home.tsx
@@ -1,23 +1,15 @@
-import dynamic from 'next/dynamic'
 import { Suspense } from 'react'
 
 import { HomePosts } from '@/components/home-posts-list/home-posts'
+import { NewPostNotification } from '@/components/new-post-notification/new-post-notification'
 import { PostPlaceholder } from '@/components/post/post-placeholder/post-placeholder'
 
 import styles from './home.module.css'
 
-const NewPostNotification = dynamic(() =>
-	import('@/components/new-post-notification/new-post-notification').then(
-		(m) => m.NewPostNotification,
-	),
-)
-
 export const Home = () => {
 	return (
 		<div className={styles.home}>
-			<Suspense fallback={null}>
-				<NewPostNotification />
-			</Suspense>
+			<NewPostNotification />
 			<Suspense fallback={<PostPlaceholder />}>
 				<HomePosts />
 			</Suspense>
