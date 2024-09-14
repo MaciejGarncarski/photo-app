9b847f02b30fecfb629cea44474dfc695a2a393a
Maciej Garncarski <maciejgarncarski@protonmail.com>
2024-09-14 12:24:37 +0200

    chore: update env

--- apps/backend/global.d.ts
+++ apps/backend/global.d.ts
@@ -5,7 +5,6 @@ declare namespace NodeJS {
 		CLOUDINARY_CLOUD_NAME: string
 		CLOUDINARY_API_KEY: string
 		CLOUDINARY_API_SECRET: string
-		CLOUDINARY_API_ENV: string
 		APP_URL: string
 		BACKEND_URL: string
 		GOOGLE_CLIENT_ID: string
