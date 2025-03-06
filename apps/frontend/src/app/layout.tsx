c82d557dc8b96daa15abb011dfbce8c74058daa6
Maciej Garncarski <maciejgarncarski@protonmail.com>
2025-03-06 18:23:51 +0100

    fix: remove font

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata, Viewport } from 'next'
-import { Lato } from 'next/font/google'
+// import { Lato } from 'next/font/google'
 import { ThemeProvider } from 'next-themes'
 import type { ReactNode } from 'react'
 
@@ -15,10 +15,10 @@ type Props = {
 	children: ReactNode
 }
 
-const font = Lato({
-	subsets: ['latin', 'latin-ext'],
-	weight: ['400', '700', '900'],
-})
+// const font = Lato({
+// 	subsets: ['latin', 'latin-ext'],
+// 	weight: ['400', '700', '900'],
+// })
 
 const description =
 	'PhotoApp is social media application created in the modern tech stack. In this app, you can chat with friends, create posts, comment on them and follow other users. It was my first attempt to create a backend in Next.js, so I learned a lot of things while creating this app.'
@@ -37,7 +37,7 @@ export const viewport: Viewport = {
 
 const DefaultLayout = ({ children }: Props) => {
 	return (
-		<html lang="en" className={font.className} suppressHydrationWarning>
+		<html lang="en" suppressHydrationWarning>
 			<head />
 			<body>
 				<PreloadResources />
